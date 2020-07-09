7abd1cfa19a9211ce1122ee8e830a6634a30b505
Geovani Pereira <geovani@govfacilbrasil.com.br>
2020-07-08 23:44:16 -0300

    Create Repositorie and GET all repositories

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require('uuidv4');
 
 // const { uuid } = require("uuidv4");
 
@@ -11,11 +12,19 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json({repositories});
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+      const title = request.body.title;
+      const url = request.body.url;
+      const techs = request.body.techs;
+      const likes = request.body.likes;
+      
+      const repositorie = { id: uuid(),title, url, techs, likes}
+      repositories.push(repositorie);
+
+      return response.json({repositorie})
 });
 
 app.put("/repositories/:id", (request, response) => {
